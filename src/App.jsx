5625d80dd3e7ff08dc814f6d760a8893db4525e7
Sarthak-Saghal <sarthaksahgal2005@gmail.com>
2025-12-02 10:39:36 +0530

    removedNavbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import Navbar from "./Components/Navbar";
 
 function App() {
   return (
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Navbar from '../Components/Navbar';
 import Sidebar from '../Components/Sidebar';
 
 export const Home = () => {
   return (
     <div className=" w-full min-h-screen flex flex-col bg-[#0D1117]">
-        <Navbar/>
-        <div className=" mt-15 flex flex-row  h-full">
+        
+        <div className=" mt-0 flex flex-row  h-full">
             <Sidebar/>
         </div>
     </div>
